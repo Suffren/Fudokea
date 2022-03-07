fb2bbf560812204cc26cd840b80e4531ce044de7
Suffren <regis.cleis@hotmail.fr>
2022-03-07 19:00:34 +0100

    Allow to add a tip

--- src/components/Tips.js
+++ src/components/Tips.js
@@ -1,27 +1,49 @@
 import { Link } from "react-router-dom";
 import { connect } from 'react-redux';
+import { useState } from 'react'
 
-function Tips({tips}) {
+function Tips({ tips, onAddTip }) {
+	const [newTip, setTip] = useState("")
+	const [tipList, setTipList] = useState(tips)
+	const submitTip = function() {
+		const tip = { title: newTip, id: tips.length + 1}
+		onAddTip(tip)
+		setTipList(tipList.concat(tip));
+		setTip('')
+	};
+	function handleChange(event) {
+		setTip(event.target.value);
+	}
 	return <div>
 		<nav>
-			{tips.map(tip =>
-				<li>
+			{tipList.map((tip, id) =>
+				<li key={id}>
 					<Link
 						to={`/tips/${tip.id}`}
 						key={tip.id}>
-							{tip.title}
+						{tip.title}
 					</Link>
 				</li>
 			)}
 		</nav>
-		
+
 		<Link to="/">Home</Link>
+		<div>
+			<input value={newTip} onChange={handleChange} type="text" />
+			<button onClick={submitTip}>Add</button>
+		</div>
 	</div>
 }
 
 const TipStore = connect(
 	state => ({
 		tips: state.tips
+	}),
+	dispatch => ({
+		onAddTip: tip => dispatch({
+			type: 'ADD_TIP_ACTION',
+			payload: { ...tip, read: false }
+		})
 	})
 )(Tips)
 export default TipStore
