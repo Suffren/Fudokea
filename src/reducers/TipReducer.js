1b025fe9274c97a216e0fcf4a12decae2c80dc62
Suffren <regis.cleis@hotmail.fr>
2022-03-07 18:59:52 +0100

    Add dispatch on Tips page and change add tip reducer

--- src/reducers/TipReducer.js
+++ src/reducers/TipReducer.js
@@ -3,24 +3,24 @@ const tips = [
 		"id": 1,
 		"title": "Title here",
 		"content": "lorem ipsum",
-        "read": true
+		"read": true
 	},
 	{
 		"id": 2,
 		"title": "Title two",
 		"content": "lorem ipsum",
-        "read": false
+		"read": false
 	}
 ]
 export const ADD_TIP_ACTION = 'ADD_TIP_ACTION';
 
-function TipReducer (state = tips, action) {
-    switch (action.type) {
-        case ADD_TIP_ACTION:
-            return []
-        default:
-            return state
-    }
+function TipReducer(state = tips, action) {
+	switch (action.type) {
+		case ADD_TIP_ACTION:
+			return [...state, { ...action.payload, content: "" }]
+		default:
+			return state
+	}
 }
 
 export default TipReducer;
