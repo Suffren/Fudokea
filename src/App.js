f4581acdb85438819b3e51d6121d27ef46ba4a36
Suffren <regis.cleis@hotmail.fr>
2022-07-09 17:19:19 +0200

    Styled the header

--- src/App.js
+++ src/App.js
@@ -12,7 +12,13 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="App">
-      <StyledHeader>FUDOKEA</StyledHeader>
+      <StyledHeader>
+        <div>FUDOKEA</div>
+        <div>
+          <span style={{ marginRight: '10px'}}><FontAwesomeIcon icon={faUser} /></span>
+          <span style={{ marginRight: '10px'}}><FontAwesomeIcon icon={faGear} /></span>
+        </div>
+      </StyledHeader>
       <div id="page">
         <Sidebar />
         <Routes>
@@ -39,7 +45,8 @@ const StyledHeader = styled.div`
   height: 60px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   border-bottom: 2px solid #ddd;
   background-color: white;
-  padding-left: 15px;
+  padding: 0px 15px;
 `;
